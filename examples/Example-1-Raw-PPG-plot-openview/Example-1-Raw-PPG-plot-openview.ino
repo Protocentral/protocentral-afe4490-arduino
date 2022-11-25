1b0c846cc67cc0ff04c59393a8b6fd5f5c1e689a
Ashwin K Whitchurch <ashwin@protocentral.com>
2022-11-25 20:58:13 +0530

    Migrated to polled mode

--- examples/Example-1-Raw-PPG-plot-openview/Example-1-Raw-PPG-plot-openview.ino
+++ examples/Example-1-Raw-PPG-plot-openview/Example-1-Raw-PPG-plot-openview.ino
@@ -29,13 +29,26 @@
 
 int datalen = 10;
 
+#define CES_CMDIF_PKT_START_1   0x0A
+#define CES_CMDIF_PKT_START_2   0xFA
+#define CES_CMDIF_TYPE_DATA   0x02
+#define CES_CMDIF_PKT_STOP    0x0B
+
 volatile char DataPacket[10];
 volatile char DataPacketFooter[2]={0x00, CES_CMDIF_PKT_STOP};
 const char DataPacketHeader[6] = {CES_CMDIF_PKT_START_1, CES_CMDIF_PKT_START_2, datalen, ((uint8_t)(datalen >> 8)), CES_CMDIF_TYPE_DATA};
 
 AFE44XX afe44xx(AFE44XX_CS_PIN, AFE44XX_PWDN_PIN, AFE44XX_DRDY_PIN, AFE44XX_INTNUM);
 
-void sendDataThroughUart(afe44xx_output_values * afe4490Data){
+afe44xx_data afe44xx_raw_data;
+uint8_t ppg_data_buff[20];
+int16_t ppg_wave_ir;
+uint16_t ppg_stream_cnt = 0;
+uint8_t sp02;
+bool ppg_buf_ready = false;
+bool spo2_calc_done = false;
+
+/*void sendDataThroughUart(afe44xx_output_values * afe4490Data){
 
   DataPacket[0] = afe4490Data->ir;
   DataPacket[1] = afe4490Data->ir>>8;
@@ -66,7 +79,7 @@ void sendDataThroughUart(afe44xx_output_values * afe4490Data){
     Serial.write(DataPacketFooter[i]);
   }
   afe4490Data->calculated_value == false;
-}
+}*/
 
 void setup()
 {
@@ -86,11 +99,37 @@ void setup()
 
 void loop()
 {
-  afe44xx_output_values afe44xxData;
-  boolean sampled_value = afe44xx.getDataIfAvailable(&afe44xxData);
+    afe44xx.get_AFE44XX_Data(&afe44xx_raw_data);
+    ppg_wave_ir = (int16_t)(afe44xx_raw_data.IR_data >> 8);
+    ppg_wave_ir = ppg_wave_ir;
 
-  if (afe44xxData.calculated_value == true)
-  {
-    sendDataThroughUart(&afe44xxData);
-  }
+    ppg_data_buff[ppg_stream_cnt++] = (uint8_t)ppg_wave_ir;
+    ppg_data_buff[ppg_stream_cnt++] = (ppg_wave_ir >> 8);
+
+    if (ppg_stream_cnt >= 19)
+    {
+      ppg_buf_ready = true;
+      ppg_stream_cnt = 0;
+    }
+
+    memcpy(&DataPacket[4], &afe44xx_raw_data.IR_data, sizeof(signed long));
+    memcpy(&DataPacket[8], &afe44xx_raw_data.RED_data, sizeof(signed long));
+
+    if (afe44xx_raw_data.buffer_count_overflow)
+    {
+
+      if (afe44xx_raw_data.spo2 == -999)
+      {
+        DataPacket[15] = 0;
+        sp02 = 0;
+      }
+      else
+      {
+        DataPacket[15] = afe44xx_raw_data.spo2;
+        sp02 = (uint8_t)afe44xx_raw_data.spo2;
+        spo2_calc_done = true;
+      }
+
+      afe44xx_raw_data.buffer_count_overflow = false;
+    }
 }
--- src/protocentral_afe44xx.h
+++ src/protocentral_afe44xx.h
@@ -1,23 +1,21 @@
 //////////////////////////////////////////////////////////////////////////////////////////
 //
-//    Arduino Library for the AFE4490 breakout board/shield from ProtoCentral Electronics
-//
-//    Author: Joice Tm
-//    Copyright (c) 2020 ProtoCentral
-//
+//    Arduino library for the AFE4490 Pulse Oxiometer Shield
+
+//    Copyright (c) 2018 ProtoCentral
 //
 //    This software is licensed under the MIT License(http://opensource.org/licenses/MIT).
 //
-//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
-//    NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-//    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-//    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-//    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+//   NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
+//   For information on how to use, visit https://github.com/Protocentral/AFE4490_Oximeter
 /////////////////////////////////////////////////////////////////////////////////////////
-
-#ifndef _AFE4490_H
-#define _AFE4490_H
+#ifndef _AFE4490
+#define _AFE4490
 
 #include "Arduino.h"
 #include <SPI.h>
@@ -25,19 +23,19 @@
 #include <math.h>
 
 //afe44xx Register definition
-#define CONTROL0    0x00
-#define LED2STC     0x01
-#define LED2ENDC    0x02
+#define CONTROL0      0x00
+#define LED2STC       0x01
+#define LED2ENDC      0x02
 #define LED2LEDSTC    0x03
 #define LED2LEDENDC   0x04
-#define ALED2STC    0x05
-#define ALED2ENDC   0x06
-#define LED1STC     0x07
-#define LED1ENDC    0x08
+#define ALED2STC      0x05
+#define ALED2ENDC     0x06
+#define LED1STC       0x07
+#define LED1ENDC      0x08
 #define LED1LEDSTC    0x09
 #define LED1LEDENDC   0x0a
-#define ALED1STC    0x0b
-#define ALED1ENDC   0x0c
+#define ALED1STC      0x0b
+#define ALED1ENDC     0x0c
 #define LED2CONVST    0x0d
 #define LED2CONVEND   0x0e
 #define ALED2CONVST   0x0f
@@ -54,71 +52,52 @@
 #define ADCRSTENDCT2  0x1a
 #define ADCRSTCNT3    0x1b
 #define ADCRSTENDCT3  0x1c
-#define PRPCOUNT    0x1d
-#define CONTROL1    0x1e
-#define SPARE1      0x1f
-#define TIAGAIN     0x20
+#define PRPCOUNT      0x1d
+#define CONTROL1      0x1e
+#define SPARE1        0x1f
+#define TIAGAIN       0x20
 #define TIA_AMB_GAIN  0x21
-#define LEDCNTRL    0x22
-#define CONTROL2    0x23
-#define SPARE2      0x24
-#define SPARE3      0x25
-#define SPARE4      0x26
-#define SPARE4      0x26
-#define RESERVED1   0x27
-#define RESERVED2   0x28
-#define ALARM     0x29
-#define LED2VAL     0x2a
-#define ALED2VAL    0x2b
-#define LED1VAL     0x2c
-#define ALED1VAL    0x2d
+#define LEDCNTRL      0x22
+#define CONTROL2      0x23
+#define SPARE2        0x24
+#define SPARE3        0x25
+#define SPARE4        0x26
+#define SPARE4        0x26
+#define RESERVED1     0x27
+#define RESERVED2     0x28
+#define ALARM         0x29
+#define LED2VAL       0x2a
+#define ALED2VAL      0x2b
+#define LED1VAL       0x2c
+#define ALED1VAL      0x2d
 #define LED2ABSVAL    0x2e
 #define LED1ABSVAL    0x2f
-#define DIAG      0x30
-#define count 60
+#define DIAG          0x30
 
-#define CES_CMDIF_PKT_START_1   0x0A
-#define CES_CMDIF_PKT_START_2   0xFA
-#define CES_CMDIF_TYPE_DATA   0x02
-#define CES_CMDIF_PKT_STOP    0x0B
-
-#define FS            25    //sampling frequency
-#define BUFFER_SIZE  (FS*4)
-#define MA4_SIZE  4 // DONOT CHANGE
-#define min(x,y) ((x) < (y) ? (x) : (y))
-
-typedef struct output_values{
+typedef struct afe44xx_Record{
   int32_t heart_rate;
   int32_t spo2;
-  signed long ir;
-  signed long red;
-  boolean calculated_value = false;
-}afe44xx_output_values;
+  signed long IR_data;
+  signed long RED_data;
+  boolean buffer_count_overflow = false;
+} afe44xx_data;
 
-void afe44xx_drdy_event();
-              
+
 class AFE44XX
 {
   public:
     AFE44XX(int cs_pin, int pwdn_pin, int drdy_pin, int intr_num);
     void afe44xx_init();
-    boolean getDataIfAvailable (afe44xx_output_values *sensed_values);  
     
+    boolean get_AFE44XX_Data (afe44xx_data *afe44xx_raw_data);
+  
   private:
-    
-    void afe44xxWrite (uint8_t address, uint32_t data);
-    void estimate_spo2(uint16_t *pun_ir_buffer, int32_t n_ir_buffer_length, uint16_t *pun_red_buffer, int32_t *pn_spo2, int8_t *pch_spo2_valid, int32_t *pn_heart_rate, int8_t *pch_hr_valid);
-    void find_peak( int32_t *pn_locs, int32_t *n_npks,  int32_t  *pn_x, int32_t n_size, int32_t n_min_height, int32_t n_min_distance, int32_t n_max_num );
-    void find_peak_above( int32_t *pn_locs, int32_t *n_npks,  int32_t  *pn_x, int32_t n_size, int32_t n_min_height);
-    void remove_close_peaks(int32_t *pn_locs, int32_t *pn_npks, int32_t *pn_x, int32_t n_min_distance);
-    void sort_ascend(int32_t  *pn_x, int32_t n_size);
-    void sort_indices_descend(  int32_t  *pn_x, int32_t *pn_indx, int32_t n_size);
+    void afe44xxWrite (uint8_t address,uint32_t data);
     unsigned long afe44xxRead (uint8_t address);
-
+    
     int _cs_pin;
     int _pwdn_pin;
     int _drdy_pin;
-    //char intrrpt_num=0;
 };
 
 #endif
